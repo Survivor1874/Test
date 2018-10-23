435af4981139d06a6760c7ae7d8b8e39b96d0e15
Lin1874 <linjun.li@quvideo.com>
2018-10-23 16:15:41 +0800

    Integer

--- src/com/lin/test/base/StringTest.java
+++ src/com/lin/test/base/StringTest.java
@@ -17,4 +17,14 @@ public class StringTest {
         String testStr = ",,,giid,ddf,,,f,,";
         System.out.println(JSON.toJSONString(Arrays.asList(testStr.split(","))));
     }
+
+    @Test
+    public void testTrim() {
+        String str1 = "";
+        String str2 = null;
+        String trim1 = str1.trim();
+        String trim2 = str2.trim();
+        System.out.println(trim1);
+        System.out.println(trim2);
+    }
 }
